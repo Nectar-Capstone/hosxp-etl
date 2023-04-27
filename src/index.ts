558be088aaee473ffe54fce598faec6bd7cc4495
Tan <pochara.youcharoen@gmail.com>
2023-04-27 15:16:59 +0700

    refactor: remove commented code

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,55 @@
 import { PrismaClient } from "@prisma/client";
 import cron from "node-cron";
-import { Kafka } from "kafkajs";
+import { Kafka, Partitioners } from "kafkajs";
 import * as dotenv from "dotenv";
 dotenv.config();
-
-// var task = cron.schedule("* * * * *", async () => {
-//   const prisma = new PrismaClient();
-//   try {
-//     const allPatient = await prisma.patient.findMany();
-//     console.log(JSON.stringify(allPatient, null, 4));
-//     await prisma.$disconnect();
-//   } catch (error) {
-//     console.log(error);
-//     await prisma.$disconnect();
-//   }
-// });
 
 const kafka = new Kafka({
   clientId: "my-app",
   brokers: ["broker:29092"],
 });
 
-const producer = kafka.producer();
+const producer = kafka.producer({
+  createPartitioner: Partitioners.LegacyPartitioner,
+});
+
+const prisma = new PrismaClient();
 
 var task = cron.schedule("* * * * *", async () => {
-  await producer.connect();
-  console.log("running cron");
-  await producer.send({
-    topic: "test4",
-    messages: [
-      {
-        value: `sent at => ${new Date().toISOString()}`,
-      },
-    ],
-  });
-  await producer.disconnect();
+  try {
+    await producer.connect();
+    console.log("running cron");
+    const allPatient = await prisma.patient.findMany();
+    console.log(JSON.stringify(allPatient, null, 4));
+    await producer.send({
+      topic: "patient1",
+      messages: [
+        {
+          value: `sent at => ${new Date().toISOString()} with value ${allPatient}`,
+        },
+      ],
+    });
+    await producer.disconnect();
+    await prisma.$disconnect();
+  } catch (error) {
+    console.log(error);
+    await prisma.$disconnect();
+  }
 });
+
+// var task = cron.schedule("* * * * *", async () => {
+//   await producer.connect();
+//   console.log("running cron");
+//   await producer.send({
+//     topic: "test4",
+//     messages: [
+//       {
+//         value: `sent at => ${new Date().toISOString()}`,
+//       },
+//     ],
+//   });
+//   await producer.disconnect();
+// });
 
 // const main = async () => {
 //   const prisma = new PrismaClient();
